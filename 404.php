014e1ef7ca25985f7fa84e767f33881b0bec24f6
tenaspace <tenaspace@gmail.com>
2024-01-13 00:08:40 +0700

    update

--- 404.php
+++ 404.php
@@ -11,15 +11,16 @@ get_header('404');
   <div class="py-20">
     <div class="<?php echo CLASSES['container']; ?>">
       <div class="flex flex-col items-center justify-center">
-        <div class="<?php echo $tw->merge([CLASSES['typography']['h1'], 'text-center text-8xl mb-4']); ?>">
+        <div class="<?php echo $tw->merge([CLASSES['typography']['h1'], 'text-center text-9xl mb-4']); ?>">
           404</div>
         <div class="<?php echo $tw->merge([CLASSES['typography']['h4'], 'text-center mb-2']); ?>">OOOps! Page not
           found</div>
         <p class="text-center mb-12">Sorry about that! Please visit our home page to get where you need to go.</p>
         <a href="<?php echo home_url(); ?>"
-          class="<?php echo $tw->merge([CLASSES['typography']['small'], 'box-border inline-block rounded-lg border border-black bg-black px-6 py-2.5 text-center text-white']); ?>">Back
-          to
-          home page</a>
+          class="box-border inline-block rounded-lg border border-black bg-black px-6 py-2.5 text-center text-white"><span
+            class="<?php echo CLASSES['typography']['small']; ?>">Back
+            to
+            home page</span></a>
       </div>
     </div>
   </div>
