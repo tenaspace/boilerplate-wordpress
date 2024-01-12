bba1f4490cd6614d81c920e429be624e3738bd0b
tenaspace <tenaspace@gmail.com>
2024-01-12 22:51:06 +0700

    update

--- page.php
+++ page.php
@@ -9,9 +9,7 @@
 <?php if (have_posts()): ?>
   <?php while (have_posts()):
     the_post(); ?>
-    <div class="the-content">
-      <?php the_content(); ?>
-    </div>
+    <?php the_content(); ?>
   <?php endwhile;
   wp_reset_postdata(); ?>
 <?php endif; ?>
--- search.php
+++ search.php
@@ -10,26 +10,18 @@
 <?php echo get_the_archive_description(); ?>
 
 <?php if (have_posts()): ?>
-  <div>
-    <div>
-      <div>
-        <?php while (have_posts()):
-          the_post(); ?>
-          <div>
-            <h2>
-              <a href="<?php echo get_the_permalink(); ?>" title="<?php echo get_the_title(); ?>">
-                <?php echo get_the_title(); ?>
-              </a>
-            </h2>
-            <p>
-              <?php echo get_the_excerpt(); ?>
-            </p>
-          </div>
-        <?php endwhile;
-        wp_reset_postdata(); ?>
-      </div>
-    </div>
-  </div>
+  <?php while (have_posts()):
+    the_post(); ?>
+    <h2>
+      <a href="<?php echo get_the_permalink(); ?>" title="<?php echo get_the_title(); ?>">
+        <?php echo get_the_title(); ?>
+      </a>
+    </h2>
+    <p>
+      <?php echo get_the_excerpt(); ?>
+    </p>
+  <?php endwhile;
+  wp_reset_postdata(); ?>
 <?php endif; ?>
 
 <?php get_footer(); ?>
