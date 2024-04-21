5d11d4cd43f9e81771937b684cfcc2b26d6d3c65
tenaspace <tenaspace@gmail.com>
2024-04-21 11:30:17 +0700

    update

--- 404.php
+++ 404.php
@@ -10,20 +10,19 @@ $dict = Dictionaries::instance()->get_scoped_i18n(['scope' => 'notFound']);
 get_header('404');
 ?>
 
-<div class="flex min-h-dvh flex-col items-center justify-center py-20 text-center"
-  style="min-height: var(--ts-window-size-height);">
+<div class="flex min-h-[var(--ts-window-size-height)] flex-col items-center justify-center py-20 text-center">
   <div class="<?php echo $ui->container(); ?>">
     <div class="flex flex-col items-center justify-center">
-      <div class="<?php echo $utils->cn($ui->typography('h1'), 'text-8xl'); ?>">
+      <div class="<?php echo $utils->cn($ui->typography('h1'), 'text-8xl font-bold'); ?>">
         404
       </div>
-      <div class="<?php echo $utils->cn($ui->typography('h2'), 'mt-4'); ?>">
+      <div class="<?php echo $utils->cn($ui->typography('h1'), 'mt-4'); ?>">
         <?php echo $dict('title'); ?>
       </div>
       <p class="mt-2">
         <?php echo $dict('description'); ?>
       </p>
-      <div class="mt-12">
+      <div class="mt-10">
         <a href="<?php echo home_url(); ?>" class="<?php echo $ui->button(); ?>">
           <?php echo $dict('backToHome'); ?>
         </a>
--- header-404.php
+++ header-404.php
@@ -14,7 +14,7 @@ $body_class = isset($args['body_class']) && is_array($args['body_class']) && siz
   <?php wp_head(); ?>
 </head>
 
-<body x-data="app" @resize.window="useWindowSize" <?php body_class($body_class); ?>
-  :style="{ '--ts-window-size-width': windowSize.width, '--ts-window-size-height': windowSize.height }">
+<body x-data="app" x-cloak @resize.window="useWindowSize" <?php body_class($body_class); ?>
+  :style="{ '--ts-window-size-height': windowSize.height }">
 
   <?php $utils->body_open(); ?>
