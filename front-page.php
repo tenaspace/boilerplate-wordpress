c893e50ff0351cc93d28ee2caa5322ced5d88d65
tenaspace <tenaspace@gmail.com>
2024-06-01 21:31:59 +0700

    update

--- front-page.php
+++ front-page.php
@@ -9,11 +9,21 @@ get_header();
 <?php if (have_posts()): ?>
   <?php while (have_posts()):
     the_post(); ?>
-    <div class="<?php echo $ui->container(); ?>">
-      <?php get_template_part('components/breadcrumb', null, ['list' => [['label' => 'Sample']]]); ?>
-      <div class="<?php echo $ui->typography(); ?>">Lorem ipsum</div>
-      <?php get_template_part('components/theme-switcher'); ?>
-      <?php get_template_part('components/forms/sample'); ?>
+    <div class='space-y-6 p-6 lg:space-y-10 lg:p-10'>
+      <h1 class='text-2xl font-extrabold tracking-tight lg:text-3xl'>boilerplate-wordpress</h1>
+      <section class='space-y-2 lg:space-y-4'>
+        <h2 class='font-bold'>Common</h2>
+        <div>
+          <div class="<?php echo $ui->container(); ?>">Container</div>
+          <div class="<?php echo $ui->typography(); ?>">Typography</div>
+          <?php get_template_part('components/theme-switcher'); ?>
+          <?php get_template_part('components/forms/sample'); ?>
+        </div>
+      </section>
+      <section class='space-y-2 lg:space-y-4'>
+        <h2 class='font-bold'>Components</h2>
+        <?php get_template_part('components/breadcrumb', null, ['list' => [['label' => 'Sample']]]); ?>
+      </section>
     </div>
     <?php the_content(); ?>
   <?php endwhile;
