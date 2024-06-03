0c357334b178ef2cb62f6bd55ee998ef5a78352f
tenaspace <tenaspace@gmail.com>
2024-06-03 08:09:05 +0700

    update

--- front-page.php
+++ front-page.php
@@ -11,19 +11,7 @@ get_header();
     the_post(); ?>
     <div class='space-y-6 p-6 lg:space-y-10 lg:p-10'>
       <h1 class='text-2xl font-extrabold tracking-tight lg:text-3xl'>boilerplate-wordpress</h1>
-      <section class='space-y-2 lg:space-y-4'>
-        <h2 class='font-bold'>Common</h2>
-        <div>
-          <div class="<?php echo $ui->container(); ?>">Container</div>
-          <div class="<?php echo $ui->typography(); ?>">Typography</div>
-          <?php get_template_part('components/theme-switcher'); ?>
-          <?php get_template_part('components/forms/sample'); ?>
-        </div>
-      </section>
-      <section class='space-y-2 lg:space-y-4'>
-        <h2 class='font-bold'>Components</h2>
-        <?php get_template_part('components/breadcrumb', null, ['list' => [['label' => 'Sample']]]); ?>
-      </section>
+      
     </div>
     <?php the_content(); ?>
   <?php endwhile;
