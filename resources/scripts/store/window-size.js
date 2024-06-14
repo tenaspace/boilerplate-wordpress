02ba9104185909a6d194f40bf7d1facf44734203
tenaspace <tenaspace@gmail.com>
2024-06-14 23:27:11 +0700

    update

--- resources/scripts/store/window-size.js
+++ resources/scripts/store/window-size.js
@@ -2,17 +2,14 @@ const WindowSize = () => {
   window.Alpine.store('windowSize', {
     width: '0px',
     height: '0px',
-    handleWindowSize() {
-      this.width = `${window.innerWidth || document.documentElement.clientWidth}px`
-      this.height = `${window.innerHeight || document.documentElement.clientHeight}px`
-    },
     init() {
-      this.handleWindowSize()
-      window.addEventListener('resize', () => {
-        this.handleWindowSize()
-      })
+      const resizeObserver = new ResizeObserver(() => {
+        this.width = `${window.innerWidth || document.documentElement.clientWidth}px`;
+        this.height = `${window.innerHeight || document.documentElement.clientHeight}px`;
+      });
+      resizeObserver.observe(document.documentElement);
     },
-  })
-}
+  });
+};
 
-export default WindowSize
+export default WindowSize;
