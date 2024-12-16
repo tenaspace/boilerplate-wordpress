4bb45c165ce0dbdc8c0cb0bab3f2fb882989924b
tenaspace <tenaspace@gmail.com>
2024-12-16 14:57:36 +0700

    update

--- resources/scripts/store/index.js
+++ resources/scripts/store/index.js
@@ -2,6 +2,12 @@ import { Theme } from './theme'
 import { WindowSize } from './window-size'
 
 const Store = () => {
+  window.Alpine.store('globals', {
+    openSidenav: false,
+    handleOpenSidenav(openSidenav) {
+      this.openSidenav = openSidenav
+    },
+  })
   Theme()
   WindowSize()
 }
