796ccb769571319de342d45ea6525495fad0f523
tenaspace <tenaspace@gmail.com>
2025-09-02 17:05:31 +0700

    update

--- src/Ajaxs/Forms/Sample.php
+++ src/Ajaxs/Forms/Sample.php
@@ -4,11 +4,15 @@ namespace TS\Ajaxs\Forms;
 class Sample
 {
   private $action;
+  private $config;
   private $response;
 
   public function __construct()
   {
     $this->action = 'sample_form';
+    $this->config = new \stdClass();
+    $this->config->send_mails = true; // SETUP
+    $this->config->submissions = true; // SETUP
     $this->response = new \stdClass();
     $this->response->send_mails = false;
     $this->response->submissions = false;
@@ -25,65 +29,86 @@ class Sample
   {
     $data = $_POST;
     if (!empty($data['nonce']) && wp_verify_nonce($data['nonce'], "{$this->action}_nonce")) {
+      unset($data['action']);
+      unset($data['nonce']);
       $token = !empty($data['token']) ? $data['token'] : '';
       $secret_key = !empty($_ENV['CLOUDFLARE_TURNSTILE_SECRET_KEY']) ? $_ENV['CLOUDFLARE_TURNSTILE_SECRET_KEY'] : '';
       $remoteip = !empty($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : (!empty($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR']);
       $turnstile = new \TS\Lib\Cloudflare\Turnstile();
       $validation = $turnstile->validate($token, $secret_key, $remoteip);
       if ($validation['success']) {
-        /**
-         * Send mails: admin
-         */
-        try {
-          $mailer = app()->lib->helpers->mailer();
-          $mailer->setFrom(!empty($_ENV['EMAIL_SENDER_ADDRESS']) ? $_ENV['EMAIL_SENDER_ADDRESS'] : '', !empty($_ENV['EMAIL_SENDER_NAME']) ? $_ENV['EMAIL_SENDER_NAME'] : '');
-          if (!empty($_ENV['EMAIL_RECIPIENTS'])) {
-            $email_recipients = explode(',', $_ENV['EMAIL_RECIPIENTS']);
-            foreach ($email_recipients as $email_recipient) {
-              $mailer->addAddress($email_recipient);
+        unset($data['token']);
+        if ($this->config->send_mails) {
+          /**
+           * Send mails: admin
+           */
+          try {
+            $mailer = app()->lib->helpers->mailer();
+            $mailer->setFrom(!empty($_ENV['EMAIL_SENDER_ADDRESS']) ? $_ENV['EMAIL_SENDER_ADDRESS'] : '', !empty($_ENV['EMAIL_SENDER_NAME']) ? $_ENV['EMAIL_SENDER_NAME'] : '');
+            if (!empty($_ENV['EMAIL_RECIPIENTS'])) {
+              $email_recipients = explode(',', $_ENV['EMAIL_RECIPIENTS']);
+              foreach ($email_recipients as $email_recipient) {
+                $mailer->addAddress($email_recipient);
+              }
             }
+            $mailer->addReplyTo(!empty($_ENV['EMAIL_REPLY_TO']) ? $_ENV['EMAIL_REPLY_TO'] : '');
+            $mailer->isHTML(true);
+            $mailer->Subject = app()->i18n->translate([
+              'en' => 'Sample form',
+              'vi' => 'Mẫu biểu mẫu',
+            ]) . ' - [#' . current_time('timestamp') . ']';
+            $mail_template_admin = new \TS\MailTemplates\Forms\Sample\Admin();
+            $mail_template_admin_html = $mail_template_admin->html($data);
+            $mailer->Body = $mail_template_admin_html;
+            $mailer->AltBody = $mail_template_admin_html;
+            $mailer->send();
+            $this->response->send_mails = true;
+          } catch (\Exception $error) {
+            app()->lib->utils->write_log($error->getMessage());
           }
-          $mailer->addReplyTo(!empty($_ENV['EMAIL_REPLY_TO']) ? $_ENV['EMAIL_REPLY_TO'] : '');
-          $mailer->isHTML(true);
-          $mailer->Subject = app()->i18n->translate([
-            'en' => 'Sample form',
-            'vi' => 'Mẫu biểu mẫu',
-          ]) . ' - [#' . current_time('timestamp') . ']';
-          $mail_template_admin = new \TS\MailTemplates\Forms\Sample\Admin();
-          $mail_template_admin_html = $mail_template_admin->html($data);
-          $mailer->Body = $mail_template_admin_html;
-          $mailer->AltBody = $mail_template_admin_html;
-          $mailer->send();
-          $this->response->send_mails = true;
-        } catch (\Exception $error) {
-          app()->lib->utils->write_log($error->getMessage());
+          /**
+           * Send mails: user
+           */
+          // if (!empty($data['email'])) {
+          //   try {
+          //     $mailer = app()->lib->helpers->mailer();
+          //     $mailer->setFrom(!empty($_ENV['EMAIL_SENDER_ADDRESS']) ? $_ENV['EMAIL_SENDER_ADDRESS'] : '', !empty($_ENV['EMAIL_SENDER_NAME']) ? $_ENV['EMAIL_SENDER_NAME'] : '');
+          //     $mailer->addAddress($data['email']);
+          //     $mailer->addReplyTo(!empty($_ENV['EMAIL_REPLY_TO']) ? $_ENV['EMAIL_REPLY_TO'] : '');
+          //     $mailer->isHTML(true);
+          //     $mailer->Subject = app()->i18n->translate([
+          //       'en' => 'Thank you',
+          //       'vi' => 'Cảm ơn',
+          //     ]);
+          //     $mail_template_user = new \TS\MailTemplates\Forms\Sample\User();
+          //     $mail_template_user_html = $mail_template_user->html($data);
+          //     $mailer->Body = $mail_template_user_html;
+          //     $mailer->AltBody = $mail_template_user_html;
+          //     $mailer->send();
+          //   } catch (\Exception $error) {
+          //     app()->lib->utils->write_log($error->getMessage());
+          //   }
+          // }
         }
-        /**
-         * Send mails: user
-         */
-        // if (!empty($data['email'])) {
-        //   try {
-        //     $mailer = app()->lib->helpers->mailer();
-        //     $mailer->setFrom(!empty($_ENV['EMAIL_SENDER_ADDRESS']) ? $_ENV['EMAIL_SENDER_ADDRESS'] : '', !empty($_ENV['EMAIL_SENDER_NAME']) ? $_ENV['EMAIL_SENDER_NAME'] : '');
-        //     $mailer->addAddress($data['email']);
-        //     $mailer->addReplyTo(!empty($_ENV['EMAIL_REPLY_TO']) ? $_ENV['EMAIL_REPLY_TO'] : '');
-        //     $mailer->isHTML(true);
-        //     $mailer->Subject = app()->i18n->translate([
-        //       'en' => 'Thank you',
-        //       'vi' => 'Cảm ơn',
-        //     ]);
-        //     $mail_template_user = new \TS\MailTemplates\Forms\Sample\User();
-        //     $mail_template_user_html = $mail_template_user->html($data);
-        //     $mailer->Body = $mail_template_user_html;
-        //     $mailer->AltBody = $mail_template_user_html;
-        //     $mailer->send();
-        //   } catch (\Exception $error) {
-        //     app()->lib->utils->write_log($error->getMessage());
-        //   }
-        // }
-        /** Submissions */
-
-        $this->response->submissions = true;
+        if ($this->config->submissions) {
+          $sheets = new \TS\Lib\Google\Apis\Sheets();
+          $service = $sheets->service();
+          $result = $service->spreadsheets_values->append(
+            !empty($_ENV['GOOGLE_SPREADSHEETS_ID_SAMPLE_FORM']) ? $_ENV['GOOGLE_SPREADSHEETS_ID_SAMPLE_FORM'] : '',
+            'Sheet1!A2',
+            new \Google\Service\Sheets\ValueRange([
+              'values' => [
+                [...array_values($data), current_time('Y-m-d H:i:s')]
+              ]
+            ]),
+            [
+              'valueInputOption' => 'RAW'
+            ]
+          );
+          if ($result->getUpdates()->getUpdatedCells() > 0) {
+            $this->response->submissions = true;
+          }
+        }
         wp_send_json(['response' => ($this->response->send_mails || $this->response->submissions)]);
       } else {
         if (is_array($validation['error-codes']) && !empty($validation['error-codes'])) {
--- src/Lib/Google/Apis/Sheets.php
+++ src/Lib/Google/Apis/Sheets.php
@@ -8,9 +8,12 @@ class Sheets
 
   }
 
-  public function auth()
+  public function service()
   {
     $client = new \Google\Client();
-
+    $client->setAuthConfig(!empty($_ENV['GOOGLE_SERVICE_ACCOUNT_CREDENTIALS']) ? json_decode($_ENV['GOOGLE_SERVICE_ACCOUNT_CREDENTIALS'], true) : '');
+    $client->addScope([\Google\Service\Sheets::SPREADSHEETS]);
+    $service = new \Google\Service\Sheets($client);
+    return $service;
   }
 }
