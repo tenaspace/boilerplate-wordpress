11c53164ce8da7a8465874d97cba03b6c8f0e987
tenaspace <tenaspace@gmail.com>
2025-11-18 21:58:28 +0700

    update

--- src/Lib/Helpers.php
+++ src/Lib/Helpers.php
@@ -65,11 +65,6 @@ class Helpers
     return $mailer;
   }
 
-  public function sendgrid()
-  {
-    return new \SendGrid(!empty($_ENV['SENDGRID_API_KEY']) ? $_ENV['SENDGRID_API_KEY'] : '');
-  }
-
   private function create_nav_menu(string $location)
   {
     if (empty($location)) {
